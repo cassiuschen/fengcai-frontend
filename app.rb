f90fd71f29d54cac16ec9c23578a19214966a8e4
cassiuschen <chzsh1995@gmail.com>
2014-11-24 20:33:04 +0800

    commit for demo online

--- app.rb
+++ app.rb
@@ -3,41 +3,42 @@
 
 #Bundler.require
 
-require 'sinatra'
+require 'sinatra/base'
 require 'sass'
 require 'coffee-script'
 require 'slim'
 
-module FengCai
-  class App < Sinatra::Base
-    # Settings
-    set :public_folder, File.dirname(__FILE__) + '/public'
-    set :views, :scss => 'assets/stylesheets', :coffee => 'assets/javascripts', :default => 'views'
-    helpers do
-      def find_template(views, name, engine, &block)
-        _, folder = views.detect { |k,v| engine == Tilt[k] }
-        folder ||= views[:default]
-        super(folder, name, engine, &block)
-      end
+
+
+class FengCaiApp < Sinatra::Base
+  # Settings
+  set :public_folder, File.dirname(__FILE__) + '/public'
+  set :port, '80'
+  set :views, :scss => 'assets/stylesheets', :coffee => 'assets/javascripts', :clim => 'views', :default => 'views'
+  helpers do
+    def find_template(views, name, engine, &block)
+      _, folder = views.detect { |k,v| engine == Tilt[k] }
+      folder ||= views[:default]
+      super(folder, name, engine, &block)
     end
-
-
-    # Assets
-    get '/assets/application.js' do
-      coffee :application
-    end
-
-    get '/assets/application.css' do
-      scss :application, :style => :expanded
-    end
-
-    # Pages
-    get '/' do
-      slim :index
-    end
-
-
-    # App
-    run! if app_file == $0
   end
+
+
+  # Assets
+  get '/assets/application.js' do
+    coffee :application
+  end
+
+  get '/assets/application.css' do
+    scss :application, :style => :expanded
+  end
+
+  # Pages
+  get '/' do
+    slim :index
+  end
+
+
+  # App
+  run! if app_file == $0
 end
