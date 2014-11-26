c3d2cd5cf009108ce1dd4dbee336201ed052822c
cassiuschen <chzsh1995@gmail.com>
2014-11-26 22:42:25 +0800

    release version.1.0-alpha

---
+++ assets/application.js
@@ -0,0 +1,230 @@
+/*
+*    Javascript For FengCai 2014
+*    Version: 1.0-alpha
+*    Code by Cassius Chen
+*    Using CoffeeScript
+*/
+(function() {
+  window.data = {
+    Player1: {
+      name: "Player1",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player2: {
+      name: "Player2",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player3: {
+      name: "Player3",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player4: {
+      name: "Player4",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player5: {
+      name: "Player5",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player6: {
+      name: "Player6",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player7: {
+      name: "Player7",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player8: {
+      name: "Player8",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player9: {
+      name: "Player9",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    },
+    Player10: {
+      name: "Player10",
+      brief: "2014-UnknowSchool-UnknowDepartment",
+      detail: "<span>他</span>，用舞蹈诠释一个个动人的故事，用歌声融合民族和流行的元素，每一次出现，都带给我们意想不到的惊喜。神秘的面具，让人惊叹的舞步，让我们在这个冬天为他的舞姿而疯狂。他就是来自中国传媒大学13广电工的胡西塔尔。 一个有着民族血液，却踩着现代舞步的哈萨克族男孩。决赛之夜，面具王子会为我们讲述怎样精彩的故事呢？让我们拭目以待!",
+      pic: "img/player_cap.jpg"
+    }
+  };
+
+  window.base = {
+    Init: function() {
+      window.base.ResetSquireDivHeight();
+      window.base.ResetInfoDivPosition();
+      window.base.ResetGoBackButtonPosition();
+      window.base.HeadRoom();
+      window.base.ChangeBackground();
+      window.base.ClickToShowModal();
+      window.base.ClicktoHideModal();
+      if (!window.base.usingComputer()) {
+        return window.mobile.Init();
+      }
+    },
+    usingComputer: function() {
+      return !(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent));
+    },
+    HeadRoom: function() {
+      console.log("headroom");
+      return $('.headroom').headroom({
+        "offset": 200,
+        "tolerance": 5,
+        "classess": {
+          "initial": "animated",
+          "pinned": "slideDown",
+          "unpinned": "slideUp"
+        }
+      });
+    },
+    DrawTab: function() {
+      var ctx, tab, tabs, _i, _len, _results;
+      tabs = $('canvas.tab');
+      _results = [];
+      for (_i = 0, _len = tabs.length; _i < _len; _i++) {
+        tab = tabs[_i];
+        ctx = tab.getContext("2d");
+        ctx.fillStyle = '#e13030';
+        _results.push(ctx.fillRect(0, 0, 30, 7));
+      }
+      return _results;
+    },
+    DrawToLine: function() {
+      var ctx, tab, tabs, _i, _len, _results;
+      tabs = $('canvas.to');
+      _results = [];
+      for (_i = 0, _len = tabs.length; _i < _len; _i++) {
+        tab = tabs[_i];
+        ctx = tab.getContext("2d");
+        ctx.strokeStyle = '#fff';
+        ctx.lineWidth = 2;
+        ctx.moveTo(0, 30);
+        ctx.lineTo(20, 0);
+        _results.push(ctx.stroke());
+      }
+      return _results;
+    },
+    ChangeData: function(params) {
+      $('#pImg').attr("src", params.pic);
+      $('#pName').text(params.name);
+      $('#pBrief').text(params.brief);
+      return $('#pDetail').html(params.detail);
+    },
+    ClickToShowModal: function() {
+      return $("[class^='player-").click(function() {
+        var rawData;
+        rawData = $(this).data("jden");
+        return window.base.ShowModal(rawData);
+      });
+    },
+    ClicktoHideModal: function() {
+      return $('#closeModal').on('click', function() {
+        return $('.modal').modal("hide");
+      });
+    },
+    ShowModal: function(playerName) {
+      this.data = window.data["" + playerName];
+      window.base.ChangeData(this.data);
+      return $('#info').modal("show");
+    },
+    ChangeBackground: function() {
+      $('.modal').on('show.bs.modal', function() {
+        return $('section').addClass('blur');
+      });
+      return $('.modal').on('hide.bs.modal', function() {
+        return $('section').removeClass('blur');
+      });
+    },
+    ResetSquireDivHeight: function() {
+      this.width = $(window).width();
+      if (this.width <= 960 && this.width > 480) {
+        $('[class^="player-"]').css("height", this.width / 2);
+      }
+      if (this.width <= 480) {
+        $('[class^="player-"]').css("height", this.width);
+      }
+      if (this.width > 960) {
+        $('[class^="player-"]').css("height", this.width / 2);
+        return $('.players [class^="player-"]').css("height", this.width / 4);
+      }
+    },
+    ResetInfoDivPosition: function() {
+      this.width = $(window).width();
+      if (this.width <= 960 && this.width > 480) {
+        $('[class^="player-"] .info').css("top", this.width / 2 - 20 - 70);
+        $('[class^="player-"] .info').css("height", 70);
+      }
+      if (this.width <= 480) {
+        $('[class^="player-"] .info').css("top", this.width - 20 - 70);
+        $('[class^="player-"] .info').css("height", 70);
+      }
+      if (this.width > 960) {
+        $('[class^="player-"] .info').css("top", this.width / 2 - 20 - 70);
+        $('[class^="player-"] .info').css("height", 70);
+        $('.players [class^="player-"] .info').css("top", this.width / 4 - 20 - 50);
+        return $('.players [class^="player-"] .info').css("height", 50);
+      }
+    },
+    ResetGoBackButtonPosition: (function(_this) {
+      return function() {
+        _this.width = $(window).width();
+        if (_this.width >= 960) {
+          $('.goBack').css("height", _this.width / 4);
+          return $('.goBack').css("width", _this.width / 4);
+        } else {
+          $(".goBack").attr("style", "");
+          return console.log("You Don't Need To Reset!!!! HAAAAAAAAAAAAAA!!!!!");
+        }
+      };
+    })(this)
+  };
+
+  window.mobile = {
+    Init: function() {
+      window.mobile.TapToShowModal();
+      return window.mobile.TapToHideModal();
+    },
+    TapToShowModal: function() {
+      var rawData;
+      rawData = $(this).data("jden");
+      return window.base.ShowModal(rawData);
+    },
+    TapToHideModal: function() {
+      return $('.modal').modal("hide");
+    }
+  };
+
+  $(function() {
+    return FastClick.attach(document.body);
+  });
+
+  $(document).ready(function() {
+    window.base.Init();
+  });
+
+  window.onresize = function() {
+    window.base.ResetSquireDivHeight();
+    window.base.ResetInfoDivPosition();
+    return window.base.ResetGoBackButtonPosition();
+  };
+
+}).call(this);
